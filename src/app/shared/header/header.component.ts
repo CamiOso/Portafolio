2a0ff876b8317151915fb5b314e090064fd75183
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-06-17 05:57:20 -0500

    Se utiliza el servicio para reemplazar informacion en la página

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { InfoPagina } from '../../interfaces/info-pagina.interface';
+import { InfoPaginaService } from '../../services/info-pagina.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  constructor(public servicio: InfoPaginaService){
+
+  }
 
 }
