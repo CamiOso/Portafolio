de4c7bb1b06499e8e3c19124984e4ea4a8c76f2f
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-08-02 18:16:55 -0500

    Se carga la información de Firebase a la pagina

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,17 +8,40 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info:InfoPagina={};
   cargada=false;
+  equipo:any[]=[];
 
 
   constructor(private http: HttpClient) {
     //console.log('Servicio de infoPagina listo');
 
+    this.cargarInfo();
+    this.cargarEquipo();
+
+
+
+   }
+
+
+   private cargarInfo(){
     this.http.get('assets/data/data-pagina.json').subscribe((resp:InfoPagina)=>{
       this.cargada=true;
       this.info=resp;
-      console.log(resp['twitter']);
+
 
 
     });
+
+
+   }
+
+
+   private cargarEquipo(){
+    this.http.get('https://angular-html-89f0a-default-rtdb.firebaseio.com/.json').subscribe((resp:any)=>{
+      this.equipo=resp;
+      console.log(this.equipo);
+
+
+    });
+
    }
 }
