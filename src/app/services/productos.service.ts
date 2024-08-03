2d9dfe9feb67ab77a8d2c701e78ae7fbfb472e7f
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-08-03 07:17:15 -0500

    Se realiza la lógica del proceso de carga y filtro

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -7,44 +7,58 @@ import { ProductoDescripcion } from '../interfaces/producto-descripcion.interfac
   providedIn: 'root'
 })
 export class ProductosService {
-  cargando=true;
+  cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
-  constructor(private http:HttpClient) {
+  constructor(private http: HttpClient) {
     this.cargarProductos();
-
   }
 
- private cargarProductos(){
-  this.cargando = true;
-  this.http.get<Producto[]>('https://angular-html-89f0a-default-rtdb.firebaseio.com/productos_idx.json')
-    .subscribe({
-      next: (resp: Producto[]) => {
+  private cargarProductos(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.cargando = true;
+      this.http.get<Producto[]>('https://angular-html-89f0a-default-rtdb.firebaseio.com/productos_idx.json')
+        .subscribe({
+          next: (resp: Producto[]) => {
+            this.productos = resp;
+            this.cargando = false;
+            resolve();
+          },
+          error: (err) => {
+            console.error('Error al cargar productos', err);
+            this.cargando = false;
+            reject(err);
+          }
+        });
+    });
+  }
 
-        this.productos = resp;
+  getProducto(id: string) {
+    return this.http.get<ProductoDescripcion>(`https://angular-html-89f0a-default-rtdb.firebaseio.com/productos/${id}.json`);
+  }
 
+  buscarProducto(termino: string) {
+    if (this.productos.length === 0) {
+      this.cargarProductos().then(() => {
+        this.filtrarProductos(termino);
+      }).catch((err) => {
+        console.error('Error al cargar productos para búsqueda', err);
+      });
+    } else {
+      this.filtrarProductos(termino);
+    }
+  }
 
+  private filtrarProductos(termino: string) {
+    this.productosFiltrado = [];
+    termino = termino.toLowerCase();
 
-          this.cargando = false;
-
-
-      },
-      error: (err) => {
-        console.error('Error al cargar productos', err);
-        this.cargando = false;
+    this.productos.forEach(prod => {
+      const tituloLower = prod.titulo.toLowerCase();
+      if (prod.categoria.toLowerCase().indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0) {
+        this.productosFiltrado.push(prod);
       }
     });
-
   }
-
-
-  getProducto(id: string){
-
-    return this.http.get<ProductoDescripcion>(`https://angular-html-89f0a-default-rtdb.firebaseio.com/productos/${id}.json`);
-
-
-  }
-
-
-
 }
