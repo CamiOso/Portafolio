61a1b3db4bf3fb1d9d73c36af941cfcaed0671cc
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-08-03 05:42:34 -0500

    Se envia parámetros por URL

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrl: './item.component.css'
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit{
+
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit()  {
+    this.route.params.subscribe(params => {
+      console.log(params['id']);
+    });
+
+
+  }
 
 }
